b310d2f2ec8b25235bdac71fec740bdcdc4cc781
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-09-26 11:42:43 -0400

    Initial Commit

---
+++ HousePoint/HousePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousePointApp.swift
+//  HousePoint
+//
+//  Created by Fatom on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct HousePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HousePointTests/HousePointTests.swift
@@ -0,0 +1,17 @@
+//
+//  HousePointTests.swift
+//  HousePointTests
+//
+//  Created by Fatom on 2025-09-26.
+//
+
+import Testing
+@testable import HousePoint
+
+struct HousePointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
